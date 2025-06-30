4dd26d2b89f8a3dfda7e6e7c6aaced66c54447fa
rafael.calderon <recalderon93@gmail.com>
2025-06-30 00:03:42 -0400

    feat: Add Landing Banners [PAN-39]

---
+++ src/i18n/translations/es/banners.ts
@@ -0,0 +1,12 @@
+export default {
+  "great-solution": {
+    badge: "Diseño y Desarrollo Personalizado",
+    "title-1": "Gran solución para quienes buscan ",
+    "title-2": "calidad alta y precios amigables para startups.",
+    cta: "Comenzar",
+  },
+  "know-more": {
+    title: "¿Quieres saber más sobre nuestros precios?",
+    cta: "Reservar una llamada",
+  },
+};
--- src/i18n/translations/es/index.ts
+++ src/i18n/translations/es/index.ts
@@ -1,10 +1,12 @@
 import footer from "./footer";
 import header from "./header";
 import hero from "./hero";
+import banners from "./banners";
 
 export default {
   greeting: "Hello, {name}!",
   footer,
   header,
   hero,
+  banners,
 };
